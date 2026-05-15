54821784117a3febbe4a83a000923945469a8188
Swiftz007 <cxllme14zx@gmail.com>
2026-05-16 01:48:00 +0700

    Modify HWID whitelist and clean up script
    Updated whitelisted HWID and removed unnecessary code.

--- HwidSystem.lua
+++ HwidSystem.lua
@@ -1,6 +1,7 @@
 local player = game:GetService("Players").LocalPlayer
 local whitelistedHWIDs = {
-    ["683c6cad96983bb60cb6b29086c0381x813c443aae5ab72f3f2abd91dae18a2a"] = true, -- เปลี่ยนเป็น HWID ของคนที่อนุญาต
+    ["683c6cad96983bb60cb6b29086c0381c811c443aae5ab72f3f2abd91dae18a2e"] = true,
+    --["xxx"] = true,
 }
 
 -- ดึงค่า HWID ปัจจุบัน
@@ -11,8 +12,3 @@ if not whitelistedHWIDs[currentHWID] then
     player:Kick("\n[ REAPER HUB ]\nFuck Hwid")
     return -- ใส่ไว้เพื่อให้สคริปต์หยุดทำงานทันทีหลังจากเตะ
 end
-
-task.wait(3)
-
--- ส่วนของสคริปต์หลัก (ถ้า HWID ตรงจะเริ่มทำงานที่นี่)
-print("Welcome to Reaper Hub!")
